e96f621de08e58d9a24a284245ae57af1a912f12
brandongatlin <brandongatlin1981@me.com>
2019-10-12 19:34:49 -0500

    added choose player, opponent, and toasts while updating screen with stats

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+
+
 const game = {
     friends: [
         {
@@ -6,6 +9,8 @@ const game = {
             'attack' : 10,
             'counter' : 15,
             'power-up' : 0,
+            'weapon' : 'Sarcasm',
+            'weakness' : 'Truth',
             'src' : './images/chandler.jpg'
         },
 
@@ -15,6 +20,8 @@ const game = {
             'attack': 5,
             'counter': 10,
             'power-up': 0,
+            'weapon': 'Looks',
+            'weakness': 'Sarcasm',
             'src': './images/joey.jpg'
         },
 
@@ -24,6 +31,8 @@ const game = {
             'attack': 25,
             'counter': 30,
             'power-up': 0,
+            'weapon': 'Truth',
+            'weakness': 'Messiness',
             'src': './images/monica.jpg'
         },
 
@@ -33,24 +42,28 @@ const game = {
             'attack': 5,
             'counter': 15,
             'power-up': 0,
+            'weapon': 'Messiness',
+            'weakness': 'Looks',
             'src': './images/jenn.jpg'
         }
     ],
 
-    player1: {},
-    opponent: {},
+    player1: null,
+    opponent: null,
+    availableFriends: [],
     defeated : [],
 
     start : function(){
-        game.player1 = game.friends[0];
-        game.createFriends()
+        game.availableFriends = game.friends;
+        game.createFriends();
+        game.showToast('Choose Your Player!');
     },
 
     createFriends : function(){
 
-        game.friends.forEach(function(friend) {
+        game.availableFriends.forEach(function(friend) {
             const newFriend = `
-                <div class="card w-50 text-center">
+                <div class="col-3 card w-50 border border-primary text-center friend" data-char=${friend.name}>
                     <div class="card-header">
                         ${friend.name}
                     </div>
@@ -61,16 +74,82 @@ const game = {
                     </div>
                 </div>
             `
-            $('#all-chars').append(newFriend);
+            $('#chars').append(newFriend);
         });
 
+    },
 
-    }
+    hideFriends : function(){
+        $('.friend').hide();
+    },
 
+    showFriends : function(){
+        
+    },
 
+    choosePlayer : function(nameDiv, hpDiv, chosen){
+
+        game.availableFriends.forEach(function(friend){
+            if(friend.name === chosen.name){
+                let idx = game.availableFriends.indexOf(friend);
+                game.availableFriends.splice(idx, 1);
+            }
+        });
+        $('#chars').empty();
+        game.createFriends();
+        game.showToast(`You chose: ${chosen.name}!`);
+        $(nameDiv).text(chosen.name);
+        $(hpDiv).text(chosen.hp);
+    },
+
+    showToast : function(text){
+        let toast = $('<div>').text(text).addClass('naZdrowie col-12');
+        $('#toast-msg').append(toast);
+        window.setTimeout(function(){
+            const toastsOnScreen = $('.naZdrowie');
+            const oldestToast = toastsOnScreen[0];
+            $(oldestToast).remove();
+        }, 2 * 1000);
+    },
 }
 
+$(document).on('click', '.friend', function(){
+    let char = $(this).data('char');
+
+    if(game.player1 === null){
+
+        game.availableFriends.forEach(function(person){
+            
+            if (person.name === char){
+                game.player1 = person;
+                game.choosePlayer('#player1-name', '#player1-hp', game.player1);
+                game.showToast(`Choose Your Enemy!`);
+            }
+        });
+        
+    } else if (game.opponent === null){
+
+        game.availableFriends.forEach(function (person) {
+
+            if (person.name === char) {
+                game.opponent = person;
+                game.choosePlayer('#opponent-name', '#opponent-hp', game.opponent);
+                game.showToast(`Let's Battle!`);
+                $('#chars').empty();
+
+            }
+        });
+
+        
+    } else {
+        console.log('players chosen');
+    }
+});
+
 game.start();
+
+});
+
 
 // functions I need:
 // choosePlayer - all 4 pix of the friends are shown and msg to pick by click. set var player1
@@ -78,3 +157,5 @@ game.start();
 // attack - will take player, opponent; will deduct hp accordingly from both characters
 // updateDisplay - will take an html id and and number - will update that display.
 // resetGame - will zero out score, call choose player
+
+// gameplay - show all friends and message to pick one. after play1 choose a friend, that friend is removed from available friends, and friendlist is re-rendered.
